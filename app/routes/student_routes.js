9a7647106b5b528b8935333bc0ca1302dcc9a29e
lanre01 <olanre12waju@gmail.com>
2017-11-05 16:08:50 +0100

    minor fix

--- app/routes/student_routes.js
+++ app/routes/student_routes.js
@@ -10,7 +10,7 @@ module.exports = function(app, db) {
 			if (err) {
 				res.send({'error':'An error has occurred'});
 			} else {
-				res.render('allstudents', {students: results});
+				res.render('allstudents', {student: results});
 			}
 		});
 	});
