864e065d0f28477347dd4ff90686a7f822af02b6
lanre01 <olanre12waju@gmail.com>
2017-11-05 23:30:58 +0100

    minor fix

--- app/routes/student_routes.js
+++ app/routes/student_routes.js
@@ -47,70 +47,39 @@ module.exports = function(app, db) {
 		});
 	});
 
-	app.get('/students/edit', function(req, res, next){
-		let regno = req.body.regno;
-		res.render('editstudent');
+	app.get('/students/edit/:regno', function(req, res, next){
+		const regno = req.params.regno;
+		db.collection('students').findOne( id,(err,item) =>{
+    if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        if(!item){
+          res.render('searchstudents', {
+        error: 'User does not exist'
+      });
+        }else {
+          res.render('editstudent',{student:item});
+        }
+      } 
+  });
 	});
 
-	app.put('/students/edit', (req, res, next) => {
+	app.post('/students/edit', (req, res, next) => {
 		const regno = {'regno': req.body.regno};
-		var firstname, lastname, email, dob, school, department,
-		sex, level, imageUrl, dateCreated, regno_old;
-		db.collection('students').findOne(regno, (err, item) => {
+		const student = {firstname: req.body.firstname,
+		 lastname: req.body.lastname, regno: req.body.regno,
+		  dob: req.body.dob, school: req.body.school,
+		   department: req.body.department, email: req.body.email,
+		    sex: req.body.sex, level: req.body.level,
+		     imageUrl: req.body.imageUrl, dateCreated: req.body.dateCreated};
+		db.collection('students').update(regno, {$set:student}, (err, result) => {
 			if (err) {
 				res.send({'error':'An error has occurred'});
 			} else {
-				if (!item) {
-					res.render('searchstudent', {error: 'User does not exit'});
-				} else {
-					firstname = item.firstname;
-					lastname = item.lastname;
-					email = item.email; dob = item.dob;
-					level = item.level; imageUrl = item.imageUrl;
-					school = item.school; sex = item.sex;
-					department = item.department;
-					dateCreated = item.dateCreated;
-					regno_old = item.regno;
+				if (result.result.nModified == 1) {
+					console.log('student data: ' + result);
+					res.render('liststudent', {student: student});
 				}
-			}
-		});
-
-		if (!(req.body.firstname == "")) {
-			firstname = req.body.firstname;
-		} if (!(req.body.lastname == "")) {
-			lastname = req.body.lastname;
-		} if (!(req.body.email == "")) {
-			email = req.body.email;
-		} if (!(req.body.dob == "")) {
-			dob = req.body.dob;
-		} if (!(req.body.department == "")) {
-			department = req.body.department;
-		} if (!(req.body.school == "")) {
-			school = req.body.school;
-		} if (!(req.body.imageUrl == "")) {
-			imageUrl = req.body.imageUrl;
-		} if (!(req.body.dateCreated == "")) {
-			dateCreated = req.body.dateCreated;
-		} if (!(req.body.sex == "")) {
-			sex = req.body.sex;
-		} if (!(req.body.regno == "")) {
-			regno_old = req.body.regno;
-		} if (!(req.body.level == "")) {
-			level = req.body.level;
-		}
-
-		const student = {firstname: firstname,
-		 lastname: lastname, regno: regno_old,
-		  dob: dob, school: school,
-		   department: department, email: email,
-		    sex: sex, level: level,
-		     imageUrl: imageUrl, dateCreated: dateCreated};
-		db.collection('students').update(regno, student, { upsert: true }, (err, result) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				console.log('student data: ' + result);
-				res.render('liststudent', {student: result});
 			}
 		});
 	});
